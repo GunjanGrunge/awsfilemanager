1bb133e0b031dd5f596f2b54f43903f32fea40f9
Gunjan <46925907+GunjanGrunge@users.noreply.github.com>
2024-12-29 12:35:10 +0530

    Update app.js
    removed require('dotenv').config();

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 const accessKeyId = process.env.ACCESS_KEY_ID;
 const secretAccessKey = process.env.SECRET_ACCESS_KEY;
 const awsmusicbackupbucket = process.env.AWS_BUCKET_NAME;
