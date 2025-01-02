9d17a8e6b950c3a1d53dc3db2fee1e4364889cf8
Gunjan <46925907+GunjanGrunge@users.noreply.github.com>
2025-01-02 10:37:24 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,35 @@
 const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: path.resolve(__dirname, 'app.js'),
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }
+    ]
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html')
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { 
+          from: path.resolve(__dirname, 'src/images'),
+          to: 'images'
+        }
+      ]
+    }),
     new webpack.DefinePlugin({
       'window.appConfig': JSON.stringify({
         AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
