30905d68dd32440566dc16cc4ba5115b098844f0
Gunjan <46925907+GunjanGrunge@users.noreply.github.com>
2025-01-02 10:29:54 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,27 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  entry: './app.js',
+  entry: './src/app.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new webpack.DefinePlugin({
-      'window.appConfig': {
-        AWS_REGION: JSON.stringify(process.env.AWS_REGION),
-        AWS_ACCESS_KEY_ID: JSON.stringify(process.env.AWS_ACCESS_KEY_ID),
-        AWS_SECRET_ACCESS_KEY: JSON.stringify(process.env.AWS_SECRET_ACCESS_KEY),
-        AWS_BUCKET_NAME: JSON.stringify(process.env.AWS_BUCKET_NAME),
+      'window.appConfig': JSON.stringify({
+        AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
+        AWS_REGION: process.env.AWS_REGION,
+        AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+        AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
         FIREBASE_CONFIG: {
-          apiKey: JSON.stringify(process.env.FIREBASE_API_KEY),
-          authDomain: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-          projectId: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-          storageBucket: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-          messagingSenderId: JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-          appId: JSON.stringify(process.env.FIREBASE_APP_ID)
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.FIREBASE_APP_ID
         }
-      }
+      })
     })
   ]
 };
