79bc28e48ea4a763f96b6467ef4425c496666c73
Gunjan <46925907+GunjanGrunge@users.noreply.github.com>
2025-01-02 16:21:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
-const config = {
-    AWS_REGION: process.env.MYAPP_AWS_REGION,
-    AWS_ACCESS_KEY_ID: process.env.MYAPP_AWS_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY: process.env.MYAPP_AWS_SECRET_ACCESS_KEY,
-    AWS_BUCKET_NAME: process.env.MYAPP_AWS_BUCKET_NAME,
+const appConfig = {
+    AWS_REGION: window.appConfig.AWS_REGION,
+    AWS_ACCESS_KEY_ID: window.appConfig.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: window.appConfig.AWS_SECRET_ACCESS_KEY,
+    AWS_BUCKET_NAME: window.appConfig.AWS_BUCKET_NAME,
     FIREBASE_CONFIG: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID,
+      apiKey: window.appConfig.FIREBASE_CONFIG.apiKey,
+      authDomain: window.appConfig.FIREBASE_CONFIG.authDomain,
+      projectId: window.appConfig.FIREBASE_CONFIG.projectId,
+      storageBucket: window.appConfig.FIREBASE_CONFIG.storageBucket,
+      messagingSenderId: window.appConfig.FIREBASE_CONFIG.messagingSenderId,
+      appId: window.appConfig.FIREBASE_CONFIG.appId,
     },
   };
-  
-  export default config;
-  
+
+window.appConfig = appConfig;
