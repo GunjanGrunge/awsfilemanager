fc752e3b680f81a517a385e244faf62bfd90a867
Gunjan <46925907+GunjanGrunge@users.noreply.github.com>
2025-01-02 15:54:51 +0530

    Add files via upload

---
+++ dist/config.js
@@ -0,0 +1,17 @@
+const config = {
+    AWS_REGION: "undefined",
+    AWS_ACCESS_KEY_ID: "undefined",
+    AWS_SECRET_ACCESS_KEY: "undefined",
+    AWS_BUCKET_NAME: "undefined",
+    FIREBASE_CONFIG: {
+      apiKey: "undefined",
+      authDomain: "undefined",
+      projectId: "undefined",
+      storageBucket: "undefined",
+      messagingSenderId: "undefined",
+      appId: "undefined",
+    },
+  };
+  
+  export default config;
+  
